222b8f8be0f4758e7ac589b41791812724977add
lhenslee <lhenslee@vols.utk.edu>
2022-06-22 16:23:26 -0700

    finished project one

--- iris/main.py
+++ iris/main.py
@@ -3,7 +3,16 @@
 # 1. Prepare Problem
 # a) Load libraries
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 # b) Load dataset
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 df = pd.read_csv('iris.csv', names=names)
@@ -14,12 +23,12 @@ print(df.shape)
 print(df.head(20))
 print(df.describe())
 # b) Data visualizations
-df.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
-#plt.show()
-df.hist()
-#plt.show()
-pd.plotting.scatter_matrix(df)
-#plt.show()
+#df.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+# plt.show()
+# df.hist()
+# plt.show()
+# pd.plotting.scatter_matrix(df)
+# plt.show()
 
 # 3. Prepare Data
 # a) Data Cleaning
@@ -28,9 +37,40 @@ pd.plotting.scatter_matrix(df)
 
 # 4. Evaluate Algorithms
 # a) Split-out validation dataset
+X = df.values[:, :4]
+y = df.values[:, 4]
+validation_size = .2
+seed = 7
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=validation_size, random_state=seed)
 # b) Test options and evaluation metric
 # c) Spot Check Algorithms
+models = [
+    ('LR', LogisticRegression(max_iter=1000)),
+    ('LDA', LinearDiscriminantAnalysis()),
+    ('KNN', KNeighborsClassifier()),
+    ('CART', DecisionTreeClassifier()),
+    ('NB', GaussianNB()),
+    ('SVM', SVC())
+]
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=seed, shuffle=True)
+    cv_results = cross_val_score(
+        model, X_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    msg = f'{name}, {cv_results.mean()}, {cv_results.std()}'
+    print(msg)
 # d) Compare Algorithms
+#fig = plt.figure()
+#fig.suptitle('Algorithm Comparison')
+#ax = fig.add_subplot(111)
+# plt.boxplot(results)
+# ax.set_xticklabels(names)
+# plt.show()
+# compare()
 
 # 5. Improve Accuracy
 # a) Algorithm Tuning
@@ -38,5 +78,11 @@ pd.plotting.scatter_matrix(df)
 
 # 6. Finalize Model
 # a) Predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+print(accuracy_score(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
 # b) Create standalone model on entire training dataset
 # c) Save model for later use
